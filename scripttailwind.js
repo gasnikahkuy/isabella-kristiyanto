9b90b49d7abfae3b2bad7c46f818a9694c2ba6d4
gasnikahkuy <105919712+gasnikahkuy@users.noreply.github.com>
2025-07-11 10:31:45 +0700

    new update

---
+++ scripttailwind.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        fontFamily: {
+          ian: ['font-ian', 'sans-serif'],
+          custom: ['font-custom', 'serif'],
+          beautiful: ['Beautiful', 'cursive'],
+        },
+      },
+    },
+  };
